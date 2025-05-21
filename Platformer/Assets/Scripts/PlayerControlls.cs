8d89e2aee34ff8e178dfdf8ca16584ead7b069de
kim-gm-it <kghamari1396@gmail.com>
2025-05-22 02:32:03 +0330

    added :  player 1 animation clips , player 1 input action System (not complete) , collider and rigid body for player1 and tilemap 1

---
+++ Platformer/Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControlls : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForse = 5f;
+    private Vector2 movementInput;
+    [SerializeField] Boolean isGrounded;
+
+    public void onJump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump Input: " + context.ReadValue<Vector2>());
+        if (context.performed && isGrounded)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up*jumpForse , ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    public void onMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move Input: " + context.ReadValue<Vector2>());
+        movementInput = context.ReadValue <Vector2>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 movement = new Vector2(movementInput.x , movementInput.y)*(moveSpeed * Time.deltaTime);
+        transform.Translate(movement);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
