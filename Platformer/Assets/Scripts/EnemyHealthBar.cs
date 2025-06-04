5bfe77a86cd7b30095bdda15bd232496a1fe471a
kim-gm-it <kghamari1396@gmail.com>
2025-06-04 11:46:09 +0330

    did a lot of things to enemies and players (felan base khaste shodam)

--- Platformer/Assets/Scripts/EnemyHealthBar.cs
+++ Platformer/Assets/Scripts/EnemyHealthBar.cs
@@ -5,20 +5,14 @@ public class EnemyHealthBar : MonoBehaviour
 {
     public Slider slider;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     public void UpdateHealthBar(float current , float max)
     {
         slider.value = current / max;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDestroy()
     {
-        
+        Destroy(gameObject);
     }
 }
