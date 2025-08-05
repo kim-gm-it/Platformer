440cae906015261b6188801a39cafefe1b6bf50c
kim-gm-it <kghamari1396@gmail.com>
2025-08-06 01:40:29 +0330

    fixed spawn manager

--- Platformer/Assets/Scripts/SpawnManager.cs
+++ Platformer/Assets/Scripts/SpawnManager.cs
@@ -2,20 +2,27 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Header("Spawn varibales")]
     public GameObject[] enemyPrefabs = new GameObject[2];
     public int maxEnemies = 6;
     public int minEnemies = 2;
     public float spawnRange = 5f ;
-    
+
+    [Header("boss references")]
+    public Transform bossTransform;
     public void SpawnEenmy()
     {
-        int numOfEnemies = Random.Range(maxEnemies, minEnemies);
+        int numOfEnemies = Random.Range(minEnemies, maxEnemies);
         int counter = 0;
 
-        GameObject enemyMinion = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
-        while(counter++ <= numOfEnemies)
+        
+        while(counter++ < numOfEnemies)
         {
-            Vector2 spawnPos = new Vector2(Random.Range(-spawnRange, spawnRange), Random.Range(-spawnRange, spawnRange));
+            //random enemy 
+            GameObject enemyMinion = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+
+            //chose a random point within a circle aroung the boss
+            Vector2 spawnPos = (Vector2)bossTransform.position + Random.insideUnitCircle * spawnRange;
 
             Instantiate(enemyMinion, spawnPos, Quaternion.identity);
         }
